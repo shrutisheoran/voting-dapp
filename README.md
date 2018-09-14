af78e1d5286a0d7420482fcb035495deac3a5f63
Saurabh Thakur <thakursaurabh1998@gmail.com>
2018-09-14 19:01:11 +0530

    Candidates structure contract created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Voting Decentralized Application
+
+> Built using Ethereum on local blockchain setup(Ganash)
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,24 @@
+// solium-disable linebreak-style
 pragma solidity ^0.4.24;
 
 contract Election {
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    // storing count of candidates
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
