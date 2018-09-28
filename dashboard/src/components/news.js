2fb12a4d31a3b213aa069bc0dd38a495c2ed8c3d
shubhamguptaji <shubham2604gupta@gmail.com>
2018-09-28 23:21:54 +0530

    news page added

--- dashboard/src/components/news.js
+++ dashboard/src/components/news.js
@@ -1,22 +1,61 @@
 import React, { Component } from "react";
+import { Row, Preloader, Col, Card, CardTitle } from "react-materialize";
 
 class News extends Component {
   state = {
     data: {}
   };
-  componentDidMount() {
+  componentWillMount() {
     fetch(
-      "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=52dea1c96275422c9d9d32ac02eb3eae&q=election"
+      "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=52dea1c96275422c9d9d32ac02eb3eae"
     )
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ data: data });
+      });
   }
   render() {
     return (
       <div>
-        <ul>
-          <li>{this.state.data.articles.title}</li>
-        </ul>
+        <Row>
+          {Object.keys(this.state.data).length === 0 && (
+            <Row style={{ margin: "10% 10% 10% 10%" }}>
+              {/* <Col s={4} m={12} /> */}
+              <Col s={4}>
+                <Preloader size="big" />
+              </Col>
+              <Col s={4} m={12} />
+            </Row>
+          )}
+          {Object.keys(this.state.data).length !== 0 && (
+            <Row>
+              <Col s={12} m={4} />
+              <Col s={12} m={8}>
+                <ul>
+                  {this.state.data.articles.map((e, index) => {
+                    return (
+                      <li key={index}>
+                        <Col m={7} s={12}>
+                          <Card
+                            horizontal
+                            header={<CardTitle image={e.urlToImage} />}
+                            actions={[
+                              <a href={e.url}>Wanna Read More! Click here</a>
+                            ]}
+                          >
+                            <p>{e.title}</p>
+                          </Card>
+                        </Col>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </Col>
+              {/* <Col s={12} m={2} /> */}
+            </Row>
+          )}
+        </Row>
       </div>
     );
   }
