2bbc5bb565f8bd50b71dba4e3703726ec3b53c34
shubhamguptaji <shubham2604gupta@gmail.com>
2018-09-29 04:28:40 +0530

    Candidate Details

--- dashboard/src/components/FrontPage.js
+++ dashboard/src/components/FrontPage.js
@@ -12,30 +12,40 @@ class FrontPage extends Component {
           </Col>
         </Row>
         <Row style={{ marginTop: "7%" }}>
-          <Col m={3} s={12} className="grid-example">
+          <Col m={4} s={12} className="grid-example">
             <Link to="/scan" className="vote-link">
               <Card className="small vote-card" />
               <div className="small-header">Vote</div>
             </Link>
           </Col>
-          <Col m={3} s={12} className="grid-example">
+          <Col m={4} s={12} className="grid-example">
             <Link to="/dashboard" className="dashboard-link">
               <Card className="small dashboard-card" />
               <div className="small-header">Dashboard</div>
             </Link>
           </Col>
-          <Col m={3} s={0} className="grid-example">
+          <Col m={4} s={0} className="grid-example">
             <Link to="/news" className="news-link">
               <Card className="small news-card" />
               <div className="small-header">News</div>
             </Link>
           </Col>
-          <Col m={3} s={0} className="grid-example">
+        </Row>
+        <Row>
+          <Col m={2} s={12} />
+          <Col m={4} s={0} className="grid-example">
             <Link to="/enrollment" className="small enrollment-card">
               <Card className="small enrollment-card" />
               <div className="small-header">Enrollment</div>
             </Link>
           </Col>
+          <Col m={4} s={0} className="grid-example">
+            <Link to="/details" className="small candidate-card">
+              <Card className="small candidate-card" />
+              <div className="small-header">Candidate Details</div>
+            </Link>
+          </Col>
+          <Col m={2} s={12} />
         </Row>
       </div>
     );
